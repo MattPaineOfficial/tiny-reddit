7715a3ce029673053c10d76ba4707da8aa8ba01f
Matt <matthewpaine0106@gmail.com>
2024-03-01 00:38:02 +0000

    Initial commit

---
+++ src/components/Article.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Article(props) {
+  return (
+    <article>
+        <a href={ "https://reddit.com" + props.article.permalink } target='_blank'>
+            <h3>{ props.article.title }</h3>
+            <p>Posted By: { props.article.author }</p>
+        </a>
+    </article>
+  )
+}
+
+export default Article;
